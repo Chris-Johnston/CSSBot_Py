c2a6842a61135c6016bebd1876758308eddfc504
Thomas Kercheval <spacekattpoispin@gmail.com>
2018-01-09 14:59:20 -0800

    Add structure for future functionality

---
+++ cogs/boolean_utils.py
@@ -0,0 +1,22 @@
+"""
+discord bot cog that is used for various number utility functions
+"""
+import discord
+from discord.ext import commands
+
+class BoolUtilsCog:
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def bool(self, ctx, exp):
+        """
+        """
+        await ctx.send(get_bool_table(exp))
+
+def setup(bot):
+    """Sets up the cog"""
+    bot.add_cog(NumberUtilsCog(bot))
+
+def get_bool_table(exp):
+    return "Test"
