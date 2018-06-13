09b61eea504dc0e180bada9ed01b3769ca0596b0
Chris Johnston <chris@thejohnstons.net>
2018-06-12 17:46:18 -0700

    Add GPA Calculator Commands, Change the command prefix (#14)
    * Add GPA conversion utility that converts points -> % and back
    
    - Adds `to_points`
    - Adds `to_percent`
    - Adds tests for this functionality
    
    * Change the command prefix from '__' to '>>'
    
    I think that this has a lot more readability, much easier to distinguish
    two '>'s compared to two '_'s

--- main.py
+++ main.py
@@ -27,14 +27,15 @@ with open('config.ini') as config_file:
 print('discordpy')
 print(discord.__version__)
 
-client = commands.Bot(command_prefix='__', description='https://github.com/Chris-Johnston/CssBot-Py')
+client = commands.Bot(command_prefix='>>', description='https://github.com/Chris-Johnston/CssBot-Py')
 
 # this is where extensions are added by default
 default_extensions = ['cogs.basic',
                       'cogs.courseInfo',
                       'cogs.number_utils',
                       'cogs.hardware_utils',
-                      'cogs.analytics']
+                      'cogs.analytics',
+                      'cogs.gpa']
 
 
 if __name__ == '__main__':
@@ -51,7 +52,7 @@ if __name__ == '__main__':
 async def on_ready():
     # print some stuff when the bot goes online
     print(f'Logged in {client.user.name} - {client.user.id}\nVersion {discord.__version__}')
-    await client.change_presence(game=discord.Game(name='Try __help'))
+    await client.change_presence(activity=discord.Game(name='Try >>help'))
 
 # now actually connect the bot
 client.run(config.get(section='Configuration', option='connection_token'),
