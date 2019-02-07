d618daaa3f2769ecab4716feea4fcbecd2bbb88e
Chris Johnston <chris@thejohnstons.net>
2019-02-06 23:55:22 -0800

    add support for looking up manpage section number

--- cogs/manpage.py
+++ cogs/manpage.py
@@ -1,8 +1,44 @@
 import discord
 from discord.ext import commands
 import re
+import subprocess
 
 parse_regex = r"^\s*([A-Za-z0-9\_\-]+)(?:(?:\s*\(?)([1-8])\)?)?\s*$"
+lookup_regex = r"^[\/\w+]+\/man\/man([1-8])\/[\w\.]+$"
+
+def lookup_manpage_section(name: str) -> int:
+    """
+    uses `man -w <name>` to look up the path of the man page,
+    then from that parses the path to get the section
+    number.
+    If not found, defaults to 1.
+
+    >>> lookup_manpage_section("ping")
+    8
+    >>> lookup_manpage_section("socket")
+    2
+    >>> lookup_manpage_section("aaaaaaa")
+    1
+    >>> lookup_manpage_section("invalid")
+    1
+    >>> lookup_manpage_section("very invalid")
+    1
+    >>> lookup_manpage_section("; /usr/bin/xed")
+    1
+
+    """
+    # prevent adding characters that might do bad things
+    protected = "&#?!@;'- |<>"
+    if any(x in name for x in protected):
+        return 1
+    result = subprocess.run(["man", "-w", name], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    # parse result.stdout
+    pattern = re.compile(lookup_regex, flags=re.I)
+    path = result.stdout.decode()
+    match = pattern.search(path)
+    if match:
+        return int(match.group(1))
+    return 1
 
 def parse(query: str) -> tuple:
     """
@@ -45,7 +81,7 @@ def parse(query: str) -> tuple:
         if section_number:
             section_number = int(section_number)
         else:
-            section_number = 1
+            section_number = lookup_manpage_section(name)
         return name, section_number
     return None
 
