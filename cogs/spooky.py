311c274198c7e11adca3bbaeeae8a9f609649ed8
Chris Johnston <chjohnston@protonmail.com>
2023-10-03 09:18:26 -0700

    market manipulation

--- cogs/spooky.py
+++ cogs/spooky.py
@@ -93,13 +93,13 @@ class SpookyMonth(commands.Cog):
         self.stonk_weight_c = random.randint(1, 10)
         self.stonk_weight_d = random.randint(1, 10)
         self.stonk_weight_e = random.randint(1, 10)
-        self.stonk_weight_f = random.randint(10, 20) / 100.0
+        self.stonk_weight_f = -random.randint(10, 20) / 100.0
     
     def get_stonk_value(self):
         # the returned value is the conversion rate between the types of coins
         # or 1 ghoul token = value skele coins
         now = datetime.datetime.now()
-        t = 0.0001 + now.day * 0.2 + now.hour + now.minute / 60.0
+        t = 0.0001 - now.day * 0.2 + now.hour + now.minute / 60.0
         value = 5.0 + self.stonk_weight_f * t + 0.5 * math.sin(self.stonk_weight_a * t) + 0.8 * math.sin(self.stonk_weight_b * t) + 0.1 * math.sin(self.stonk_weight_c * t) + 2 * math.sin(t / self.stonk_weight_d) + 2 * math.cos(t / self.stonk_weight_e)
         return max(0.0001, value)
     
